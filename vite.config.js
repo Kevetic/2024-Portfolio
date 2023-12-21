aedc821e8165621e65d6d9a0b4f3fc83c2f2c58a
Kevetic <Kevetic@gmail.com>
2023-12-20 22:18:31 -0600

    ghpages

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ import tailwindcss from "tailwindcss";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  base: ["/2024-portfolio"],
   css: {
     postcss: {
       plugins: [tailwindcss()],
